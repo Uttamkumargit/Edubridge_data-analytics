24a0aec526e0e0b5b1e61899bd841fadcce6da80
Uttamkumargit <uttam.k7052568@gmail.com>
2022-01-22 17:34:16 +0530

    Update Assignment3

--- ASSIGNMENTS/Assignment3/subtraction of two matrix.R
+++ ASSIGNMENTS/Assignment3/subtraction of two matrix.R
@@ -1,7 +1,7 @@
-# Multiplication of two matrix
+# Subtraction of two matrix
 m1<-matrix(1:9,nrow = 3, byrow = TRUE) # first matrix
 
 m2<-matrix(17:25,nrow = 3,byrow=TRUE) #second matrix
 
-m3 <- m1*m2 #Multipliction two matrices
+m3 <- m1-m2 #Multipliction two matrices
 print(m3)
